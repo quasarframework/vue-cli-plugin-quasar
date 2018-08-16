a1c31b6df835a2eb350b484c6a31d7fc7055da81
GastroGeek <gastrogeek@gmail.com>
2018-08-16 11:13:31 +0100

    Refactored imports and Vue.use() based on prompt responses. Added transform-imports to Babel config.

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 module.exports = (api, options) => {
   const
     theme = options.pluginOptions.quasar.theme,
-    rtl = options.pluginOptions.quasar.rtlSupport
+    rtl = options.pluginOptions.quasar.rtlSupport,
+    all = options.pluginOptions.quasar.framework === 'all'
 
   if (rtl) {
     process.env.QUASAR_RTL = true
   }
 
   api.chainWebpack(chain => {
+    chain.module.rule('babel')
+      .test(/\.jsx?$/)
+      .exclude
+        .add(filepath => {
+          // always transpile js(x) in Vue files
+          if (/\.vue\.jsx?$/.test(filepath)) {
+            return false
+          }
+
+          if ([/[\\/]node_modules[\\/]quasar-framework[\\/]/].some(dep => filepath.match(dep))) {
+            return false
+          }
+
+          // don't transpile anything else in node_modules
+          return /[\\/]node_modules[\\/]/.test(filepath)
+        })
+        .end()
+      .use('babel-loader')
+      .loader('babel-loader')
+        .options({
+          extends: api.resolve('babel.config.js'),
+          plugins: !all ? [
+            [
+              'transform-imports', {
+                quasar: {
+                  transform: `quasar-framework/dist/babel-transforms/imports.${theme}.js`,
+                  preventFullImport: true
+                }
+              }
+            ]
+          ] : []
+        })
+
     chain
       .resolve
         .alias
